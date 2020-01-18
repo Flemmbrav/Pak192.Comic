c7309cb6216700254fb1103a201045f349c5ca99
Leartin <leartin.dialonis@gmail.com>
2020-01-18 11:36:06 +0100

    ADD: P192C theme

---
+++ COMPILE_THEME.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo 'pak192.comic open-source repository compiler for Linux'
+echo -e '======================================================\n'
+
+echo 'This bash compiles the theme into a new folder'
+echo -e 'called themes, makeobj must be in root folder.\n'
+
+echo -n 'Checking for makeobj... '
+
+if [ ! -f 'makeobj' ]; then
+    echo 'ERROR: makeobj not found in root folder.'
+    exit 1
+fi
+
+echo -e 'OK\n'
+
+mkdir themes
+./makeobj pak64 themes/menu.pak192comic.pak theme/pak192comic/theme.dat
+./makeobj pak64 themes/menu.pak192comicxxl.pak theme/pak192comicxxl/theme.dat
+cp -v ./theme/*.tab ./themes/ 
+zip -r themes.zip themes
+
+
+echo '======================================================'
+echo 'theme complete!'
+echo '======================================================'
