b68b67194d7d92996c74e0085e95226d3c7b9bf8
Flemmbrav <nshplus@gmail.com>
2021-11-10 19:38:43 +0100

    got rid of the csv

--- Compile_Converted.sh
+++ Compile_Converted.sh
@@ -42,83 +42,88 @@ compile() {
     for dat in $3; do
 
         if [ -f "$dat" ] ; then
-        # get directory where the dat file is located
-            local dir=$(dirname "$dat")
 
-            # hash the dat file
-            local IFS=' '
-            local dathash=($(sha256sum "$dat"))
-            local dathash=$dathash
-            local IFS=,
-
-            # obtain all image names inside the dat file
-            local images=$(awk -F= '
-                BEGIN {
-                    IGNORECASE=1
-                }
-                {
-                    if (/^([^#]*image\[|cursor|icon)/) {
-                        match($2, /(\.+[\/\\])*[a-z0-9\/\-_\\()]+/);
-                        images[substr($2, RSTART, RLENGTH)]++
-                    }
-                }
-                END {
-                    for (img in images) {
-                        if (img != "-") {
-                            printf "'$dir'/%s.png,", img
-                        }
-                    }
-                }' "$dat")
-
-            if [[ ! -z $images ]]; then
-                # hash all the images
-                tempsha=$(sha256sum $images)
-
-                if [[ $? != 0 ]]; then
-                    echo -e "\x1B[33mWarning: Failed to get one or more hashes on $dat\x1B[0m"
-                fi
-
-                local imghash=($(printf '%s %s\n' $tempsha | awk '{ printf "%s,", $1 }'))
-            fi
-
-            # get the hashes from the previous run
-            local validate=($(awk -F, "\$1 == \"$dat\"" "$csv"))
-
-            # assume no recompilation necessary
-            local recompile=0
-
-            # check hashes and number of images
-            if [[ $dathash != ${validate[1]} || $((${#validate[*]} - 2)) != ${#imghash[*]} ]]; then
-                local recompile=1
-            else
-                # check all image hashes
-                for hash in ${imghash[*]}; do
-                    if [[ ! "${validate[*]}" =~ $hash ]]; then
-                        local recompile=1
-                    fi
-                done
-            fi
-
-            # recompiling if necessary
-            if [[ $recompile == 1 ]]; then
-                ./makeobj pak$1 ./compiled_converted/ "./$dat" &> /dev/null
+            ./makeobj pak$1 ./compiled_converted/ "./$dat" &> /dev/null
                 if [[ $? != 0 ]]; then
                     echo "Error: Makeobj returned an error for $dat. Aborting..."
-    		set -e o pipefail
-                    echo -e "\x1B[33mError: Can not compile $dat\x1B[0m"
-                    rm "$csv.in"
-                    exit $?
                 fi
-            fi
+        fi
 
-            # put the hashes in the $csv.in file
-            echo "$dat,$dathash,${imghash[*]}" >> "$csv.in"
+        # get directory where the dat file is located
+        #    local dir=$(dirname "$dat")
+
+            # hash the dat file
+        #    local IFS=' '
+         #   local dathash=($(sha256sum "$dat"))
+          #  local dathash=$dathash
+           # local IFS=,
+
+            # obtain all image names inside the dat file
+           # local images=$(awk -F= '
+            #    BEGIN {
+             #       IGNORECASE=1
+              #  }
+               # {
+                #    if (/^([^#]*image\[|cursor|icon)/) {
+                 #       match($2, /(\.+[\/\\])*[a-z0-9\/\-_\\()]+/);
+                  #      images[substr($2, RSTART, RLENGTH)]++
+        #            }
+         #       }
+          #      END {
+           #         for (img in images) {
+            #            if (img != "-") {
+             #               printf "'$dir'/%s.png,", img
+              #          }
+               #     }
+   #             }' "$dat")
+#
+ #           if [[ ! -z $images ]]; then
+  #              # hash all the images
+   #             tempsha=$(sha256sum $images)
+#
+ #               if [[ $? != 0 ]]; then
+  #                  echo -e "\x1B[33mWarning: Failed to get one or more hashes on $dat\x1B[0m"
+   #             fi
+#
+ #               local imghash=($(printf '%s %s\n' $tempsha | awk '{ printf "%s,", $1 }'))
+  #          fi
+#
+ #           # get the hashes from the previous run
+  #          local validate=($(awk -F, "\$1 == \"$dat\"" "$csv"))
+#
+ #           # assume no recompilation necessary
+  #          local recompile=0
+#
+ #           # check hashes and number of images
+  #          if [[ $dathash != ${validate[1]} || $((${#validate[*]} - 2)) != ${#imghash[*]} ]]; then
+   #             local recompile=1
+    #        else
+     #           # check all image hashes
+      #          for hash in ${imghash[*]}; do
+       #             if [[ ! "${validate[*]}" =~ $hash ]]; then
+        #                local recompile=1
+         #           fi
+          #      done
+           # fi
+#
+ #           # recompiling if necessary
+  #          if [[ $recompile == 1 ]]; then
+   #             
+    #		set -e o pipefail
+    #                echo -e "\x1B[33mError: Can not compile $dat\x1B[0m"
+    #                rm "$csv.in"
+    #                exit $?
+    #            fi
+    #        fi
+
+    #        # put the hashes in the $csv.in file
+     #       echo "$dat,$dathash,${imghash[*]}" >> "$csv.in"
 
             #if [ $TERM ] ; then
             #    progressbar $index $size $dat
             #fi
-            local index=$(( $index + 1 ))
-        fi
+      #      local index=$(( $index + 1 ))
+      #  fi
     done
 
     # jump line because of progress bar
