2c461fcce37574c1a7cadf077bf825ce05b09969
Quantumzhao <quantumzhao@protonmail.ch>
2023-08-28 20:46:34 -0400

    reorganized project files

--- compile_ng_ele.sh
+++
@@ -1,3 +0,0 @@
-./DatConverter.sh &&
-./makeobj pak192 ./compiled_converted/ calculated/pakset/infrastructure/narrowgauge/ng_elevated.dat &&
-cp ./compiled_converted/way.narrowgauge_elevated.pak ~/simutrans/GameFiles/compiled_converted/
